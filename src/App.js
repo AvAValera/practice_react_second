4f798066826cc6832acbd6867fc8a9042d2b8098
AvAValera <avavarenik07@gmail.com>
2021-06-11 16:31:22 +0300

    start practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cars from './Cars';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {cars: [
+        {name: 'Ford', year: 2016, color: 'red'},
+        {name: 'Opel', year: 2021, color: 'blue'},
+        {name: 'Bmw', year: 2010, color: 'green'},
+        ],
+        pageTitle: 'Cars',
+        change: false
+    }
+    clicker = (newTitle) => {
+        this.state.change ? this.setState({
+            pageTitle : newTitle, 
+            change: false
+        }):
+        this.setState({
+            pageTitle : newTitle, 
+            change : true
+        })
+    }
+    changeInput = (e) => {
+        this.setState({
+            pageTitle: e.target.value
+        })
+    }
+    render() {
+        const cars = this.state.cars
+        return (
+            <div className = 'App'>
+                <h1 style={{color: 'blue'}}>{this.state.pageTitle}</h1>
+                <input type="text" onChange={this.changeInput} />
+                <Cars name={cars[0].name} year={cars[0].year} color={cars[0].color} changers = {this.clicker.bind(this, cars[0].name)}/>
+                <Cars name={cars[1].name} year={cars[1].year} color={cars[1].color} changers = {this.clicker.bind(this, cars[1].name)}/>
+                <Cars name={cars[2].name} year={cars[2].year} color={cars[2].color} changers = {this.clicker.bind(this, cars[2].name)}/>
+            </div>
+        )
+    }
 }
 
-export default App;
